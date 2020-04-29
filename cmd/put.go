c76771572f003334b5b0cdccca98f94ab122d8c1
Yang Zhao <jszyyx@163.com>
2020-04-29 14:45:43 +0800

    add md5 path

--- cmd/put.go
+++ cmd/put.go
@@ -1,15 +1,19 @@
 package cmd
 
 import (
+	"crypto/md5"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	. "karst/config"
+	"karst/util"
 	"math"
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -27,12 +31,33 @@ var putCmd = &cobra.Command{
 		// Read configuration
 		ReadConfig()
 
+		timeStart := time.Now()
 		// Get base file information
 		file, err := os.Open(args[0])
 		if err != nil {
 			panic(fmt.Errorf("Fatal error in opening '%s': %s\n", args[0], err))
 		}
 		defer file.Close()
+
+		// TODO: Ensure that the same file and block are not duplicated
+		md5hash := md5.New()
+		if _, err = io.Copy(md5hash, file); err != nil {
+			panic(fmt.Errorf("Fatal error in calculating md5 of '%s': %s\n", args[0], err))
+		}
+
+		if _, err = file.Seek(0, 0); err != nil {
+			panic(fmt.Errorf("Fatal error in seek '%s': %s\n", args[0], err))
+		}
+
+		fileStorePath := filepath.FromSlash(Config.FilesPath + "/" + hex.EncodeToString(md5hash.Sum(nil)))
+		if util.IsDirOrFileExist(fileStorePath) {
+			fmt.Printf("This '%s' already be stored\n", args[0])
+			return
+		} else {
+			if err := os.MkdirAll(fileStorePath, os.ModePerm); err != nil {
+				panic(fmt.Errorf("Fatal error in creating file store directory: %s\n", err))
+			}
+		}
 
 		fileInfo, err := file.Stat()
 		if err != nil {
@@ -53,7 +78,8 @@ var putCmd = &cobra.Command{
 
 			// Write to disk
 			partHash := sha256.Sum256(partBuffer)
-			partFileName := filepath.FromSlash(Config.FilesPath + "/" + strconv.FormatUint(i, 10) + "-" + hex.EncodeToString(partHash[:]))
+			partHashString := hex.EncodeToString(partHash[:])
+			partFileName := filepath.FromSlash(fileStorePath + "/" + strconv.FormatUint(i, 10) + "-" + partHashString)
 
 			if _, err = os.Create(partFileName); err != nil {
 				panic(fmt.Errorf("Fatal error in creating the part '%s' of '%s': %s\n", partFileName, args[0], err))
@@ -62,10 +88,8 @@ var putCmd = &cobra.Command{
 			if err = ioutil.WriteFile(partFileName, partBuffer, os.ModeAppend); err != nil {
 				panic(fmt.Errorf("Fatal error in writing the part '%s' of '%s': %s\n", partFileName, args[0], err))
 			}
-
-			fmt.Printf("Split the %d part into '%s' \n", i, partFileName)
 		}
 
-		fmt.Printf("Put '%s' successfully!\n", args[0])
+		fmt.Printf("Put '%s' successfully in %s !\n", args[0], time.Since(timeStart))
 	},
 }
