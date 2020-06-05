f5850f7677d34e2bd9514f129023638ec57b12b9
LowEntropyBody <jszyyx@163.com>
2020-06-05 15:47:51 +0800

    Split interface (#25)
    * base split wscmd
    
    * split func
    
    * readme
    
    * remove useless code

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -47,6 +47,7 @@ var daemonCmd = &cobra.Command{
 			putWsCmd,
 			getWsCmd,
 			registerWsCmd,
+			splitWsCmd,
 		}
 
 		for _, wsCmd := range wsCommands {
@@ -56,8 +57,8 @@ var daemonCmd = &cobra.Command{
 		// Start websocket service
 		if err := ws.StartServer(db, cfg); err != nil {
 			logger.Error("%s", err)
+		} else {
+			logger.Info("Karst daemon successfully!")
 		}
-
-		logger.Info("Karst daemon successfully!")
 	},
 }
---
+++ cmd/split.go
@@ -0,0 +1,184 @@
+package cmd
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"karst/config"
+	"karst/logger"
+	"karst/merkletree"
+	"karst/model"
+	"karst/util"
+	"karst/wscmd"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/cheggaaa/pb"
+	"github.com/spf13/cobra"
+)
+
+type SplitReturnMsg struct {
+	Info       string `json:"info"`
+	MerkleTree string `json:"merkle_tree"`
+	Status     int    `json:"status"`
+}
+
+func init() {
+	splitWsCmd.ConnectCmdAndWs()
+	rootCmd.AddCommand(splitWsCmd.Cmd)
+}
+
+var splitWsCmd = &wscmd.WsCmd{
+	Cmd: &cobra.Command{
+		Use:   "split [file_path] [output_path]",
+		Short: "Split file to merkle tree structure",
+		Long:  "Split file to merkle tree structure, splited files will be saved in output_path/root_hash/",
+		Args:  cobra.MinimumNArgs(2),
+	},
+	Connecter: func(cmd *cobra.Command, args []string) (map[string]string, error) {
+		reqBody := map[string]string{
+			"file_path":   args[0],
+			"output_path": args[1],
+		}
+
+		return reqBody, nil
+	},
+	WsEndpoint: "split",
+	WsRunner: func(args map[string]string, wsc *wscmd.WsCmd) interface{} {
+		timeStart := time.Now()
+
+		// Check input
+		filePath := args["file_path"]
+		if filePath == "" {
+			errString := "File path is needed"
+			logger.Error(errString)
+			return SplitReturnMsg{
+				Info:   errString,
+				Status: 400,
+			}
+		}
+
+		outputPath := strings.TrimRight(strings.TrimRight(args["output_path"], "/"), "\\")
+		if outputPath == "" {
+			errString := "Output path is needed"
+			logger.Error(errString)
+			return SplitReturnMsg{
+				Info:   errString,
+				Status: 400,
+			}
+		}
+
+		fileInfo, err := splitFile(filePath, outputPath, wsc.Cfg)
+		if err != nil {
+			logger.Error("%s", err)
+			fileInfo.ClearFile()
+			return PutReturnMessage{
+				Info:   err.Error(),
+				Status: 500,
+			}
+		}
+
+		merkleTreeBytes, _ := json.Marshal(fileInfo.MerkleTree)
+		logger.Debug("Splited merkleTree is %s", string(merkleTreeBytes))
+
+		returnInfo := fmt.Sprintf("Split '%s' successfully in %s ! It root hash is '%s'.", filePath, time.Since(timeStart), fileInfo.MerkleTree.Hash)
+		logger.Info(returnInfo)
+		return SplitReturnMsg{
+			Info:       returnInfo,
+			MerkleTree: string(merkleTreeBytes),
+			Status:     200,
+		}
+	},
+}
+
+func splitFile(filePath string, outputPath string, cfg *config.Configuration) (*model.FileInfo, error) {
+	// Create file information class
+	fileInfo := &model.FileInfo{
+		StoredPath:       "",
+		MerkleTree:       nil,
+		MerkleTreeSealed: nil,
+	}
+
+	// Open file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fileInfo, fmt.Errorf("Fatal error in opening '%s': %s", filePath, err)
+	}
+	defer file.Close()
+
+	// Create file directory
+	fileStorePathInBegin := filepath.FromSlash(outputPath + "/" + strconv.FormatInt(time.Now().UnixNano(), 10))
+	if err := os.MkdirAll(fileStorePathInBegin, os.ModePerm); err != nil {
+		return fileInfo, fmt.Errorf("Fatal error in creating file store directory: %s", err)
+	} else {
+		fileInfo.StoredPath = fileStorePathInBegin
+	}
+
+	fileStat, err := file.Stat()
+	if err != nil {
+		return fileInfo, fmt.Errorf("Fatal error in getting '%s' information: %s", filePath, err)
+	}
+
+	// Split file
+	totalPartsNum := uint64(math.Ceil(float64(fileStat.Size()) / float64(cfg.FilePartSize)))
+	partHashs := make([][]byte, 0)
+	partSizes := make([]uint64, 0)
+
+	logger.Info("Splitting '%s' to %d parts.", filePath, totalPartsNum)
+	bar := pb.StartNew(int(totalPartsNum))
+	for i := uint64(0); i < totalPartsNum; i++ {
+		// Bar
+		bar.Increment()
+
+		// Get part of file
+		partSize := int(math.Min(float64(cfg.FilePartSize), float64(fileStat.Size()-int64(i*cfg.FilePartSize))))
+		partBuffer := make([]byte, partSize)
+
+		if _, err = file.Read(partBuffer); err != nil {
+			return fileInfo, fmt.Errorf("Fatal error in getting part of '%s': %s", filePath, err)
+		}
+
+		// Get part information
+		partHash := sha256.Sum256(partBuffer)
+		partHashs = append(partHashs, partHash[:])
+		partSizes = append(partSizes, uint64(partSize))
+		partHashString := hex.EncodeToString(partHash[:])
+		partFileName := filepath.FromSlash(fileInfo.StoredPath + "/" + strconv.FormatUint(i, 10) + "_" + partHashString)
+
+		// Write to disk
+		partFile, err := os.Create(partFileName)
+		if err != nil {
+			return fileInfo, fmt.Errorf("Fatal error in creating the part '%s' of '%s': %s", partFileName, filePath, err)
+		}
+
+		if _, err = partFile.Write(partBuffer); err != nil {
+			return fileInfo, fmt.Errorf("Fatal error in writing the part '%s' of '%s': %s", partFileName, filePath, err)
+		}
+		partFile.Close()
+	}
+	bar.Finish()
+
+	// Rename folder
+	fileMerkleTree := merkletree.CreateMerkleTree(partHashs, partSizes)
+	fileStorePathInHash := filepath.FromSlash(outputPath + "/" + fileMerkleTree.Hash)
+
+	if !util.IsDirOrFileExist(fileStorePathInHash) {
+		if err = os.Rename(fileInfo.StoredPath, fileStorePathInHash); err != nil {
+			return fileInfo, fmt.Errorf("Fatal error in renaming '%s' to '%s': %s", fileInfo.StoredPath, fileStorePathInHash, err)
+		} else {
+			fileInfo.StoredPath = fileStorePathInHash
+		}
+	} else {
+		os.RemoveAll(fileInfo.StoredPath)
+		fileInfo.StoredPath = fileStorePathInHash
+	}
+
+	fileInfo.MerkleTree = fileMerkleTree
+
+	return fileInfo, nil
+}
