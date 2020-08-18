aec16bdd2067659d17f4ea74badde4873897f73d
Yang Zhao <jszyyx@163.com>
2020-08-18 17:32:10 +0800

    realize ipfs interface

--- filesystem/ipfs.go
+++ filesystem/ipfs.go
@@ -1,7 +1,10 @@
 package filesystem
 
 import (
+	"fmt"
+	"io/ioutil"
 	"karst/config"
+	"os"
 
 	shell "github.com/ipfs/go-ipfs-api"
 )
@@ -11,7 +14,39 @@ type Ipfs struct {
 }
 
 func OpenIpfs(cfg *config.Configuration) (*Ipfs, error) {
-	return &Ipfs{
-		sh: shell.NewShell(cfg.Fs.Ipfs.BaseUrl),
-	}, nil
+	sh := shell.NewShell(cfg.Fs.Ipfs.BaseUrl)
+	if sh.IsUp() {
+		return nil, fmt.Errorf("Ipfs dosen't start")
+	}
+	return &Ipfs{sh: sh}, nil
+}
+
+func (this *Ipfs) Close() {
+
+}
+
+func (this *Ipfs) Put(fileName string) (string, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	return this.sh.Add(f)
+}
+
+func (this *Ipfs) Get(key string, outFileName string) error {
+	return this.sh.Get(key, outFileName)
+}
+
+func (this *Ipfs) Delete(key string) error {
+	return this.sh.Unpin(key)
+}
+
+func (this *Ipfs) GetToBuffer(key string, size uint64) ([]byte, error) {
+	dataReader, err := this.sh.Cat(key)
+	if err != nil {
+		return nil, err
+	}
+	defer dataReader.Close()
+	return ioutil.ReadAll(dataReader)
 }
