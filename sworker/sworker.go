0254cc9a6fcfa8b4ced7ac35446ef29946ca88c9
Yang Zhao <jszyyx@163.com>
2020-08-20 10:33:06 +0800

    use http to seal and unseal

--- sworker/sworker.go
+++ sworker/sworker.go
@@ -10,8 +10,6 @@ import (
 	"karst/merkletree"
 	"net/http"
 	"time"
-
-	"github.com/gorilla/websocket"
 )
 
 type sealedMessage struct {
@@ -26,44 +24,50 @@ type unsealBackMessage struct {
 	Path   string
 }
 
-// TODO: change to wss
 func Seal(sworker *config.SworkerConfiguration, path string, merkleTree *merkletree.MerkleTreeNode) (*merkletree.MerkleTreeNode, string, error) {
-	// Connect to sworker
-	url := sworker.WsBaseUrl + "/api/v0/storage/seal"
-	c, _, err := websocket.DefaultDialer.Dial(url, nil)
-	if err != nil {
-		return nil, "", err
-	}
-	defer c.Close()
-
-	// Send file to seal
+	// Generate request
+	url := sworker.HttpBaseUrl + "/api/v0/storage/seal"
 	reqBody := map[string]interface{}{
-		"backup": sworker.Backup,
-		"body":   merkleTree,
-		"path":   path,
+		"body": merkleTree,
+		"path": path,
 	}
 
-	reqBodyBytes, err := json.Marshal(reqBody)
-	if err != nil {
-		return nil, "", err
-	} else {
-		logger.Debug("Request body for sealing: %s", string(reqBodyBytes))
-	}
+	reqBodyBytes, _ := json.Marshal(reqBody)
 
-	err = c.WriteMessage(websocket.TextMessage, reqBodyBytes)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBodyBytes))
 	if err != nil {
 		return nil, "", err
 	}
 
-	// Deal result
-	_, message, err := c.ReadMessage()
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("backup", sworker.Backup)
+
+	// Request
+	client := &http.Client{
+		Timeout: 20 * time.Second,
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+		},
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, "", err
 	}
-	logger.Debug("Recv: %s", message)
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		returnBody, _ := ioutil.ReadAll(resp.Body)
+		return nil, "", fmt.Errorf("Request seal failed, error is: %s, error code is: %d", string(returnBody), resp.StatusCode)
+	}
+
+	returnBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, "", err
+	}
 
 	var sealedMsg sealedMessage
-	err = json.Unmarshal([]byte(message), &sealedMsg)
+	err = json.Unmarshal([]byte(returnBody), &sealedMsg)
 	if err != nil {
 		return nil, "", fmt.Errorf("Unmarshal seal result failed: %s", err)
 	}
@@ -81,41 +85,48 @@ func Seal(sworker *config.SworkerConfiguration, path string, merkleTree *merklet
 }
 
 func Unseal(sworker *config.SworkerConfiguration, path string) (*merkletree.MerkleTreeNode, string, error) {
-	// Connect to sworker
-	url := sworker.WsBaseUrl + "/api/v0/storage/unseal"
-	c, _, err := websocket.DefaultDialer.Dial(url, nil)
-	if err != nil {
-		return nil, "", err
-	}
-	defer c.Close()
-
-	// Send file to unseal
+	// Generate request
+	url := sworker.HttpBaseUrl + "/api/v0/storage/unseal"
 	reqBody := map[string]interface{}{
-		"backup": sworker.Backup,
-		"path":   path,
+		"path": path,
 	}
 
-	reqBodyBytes, err := json.Marshal(reqBody)
-	if err != nil {
-		return nil, "", err
-	} else {
-		logger.Debug("Request body for unsealing: %s", string(reqBodyBytes))
-	}
+	reqBodyBytes, _ := json.Marshal(reqBody)
 
-	err = c.WriteMessage(websocket.TextMessage, reqBodyBytes)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBodyBytes))
 	if err != nil {
 		return nil, "", err
 	}
 
-	// Deal result
-	_, message, err := c.ReadMessage()
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("backup", sworker.Backup)
+
+	// Request
+	client := &http.Client{
+		Timeout: 20 * time.Second,
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+		},
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, "", err
 	}
-	logger.Debug("Recv: %s", message)
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		returnBody, _ := ioutil.ReadAll(resp.Body)
+		return nil, "", fmt.Errorf("Request unseal failed, error is: %s, error code is: %d", string(returnBody), resp.StatusCode)
+	}
+
+	returnBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, "", err
+	}
 
 	var unsealBackMes unsealBackMessage
-	err = json.Unmarshal([]byte(message), &unsealBackMes)
+	err = json.Unmarshal([]byte(returnBody), &unsealBackMes)
 	if err != nil {
 		return nil, "", fmt.Errorf("Unmarshal unseal back message failed: %s", err)
 	}
